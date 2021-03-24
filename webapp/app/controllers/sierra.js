c3f413ab6f4d8420e35b583090d83d9e3a906118
weddled <59847977+weddled@users.noreply.github.com>
2021-03-24 13:01:53 -0400

    run back

--- webapp/app/controllers/sierra.js
+++ webapp/app/controllers/sierra.js
@@ -3,35 +3,39 @@ const sierra = require('../dbs/sierra')
 
 const getITypes = () => {
   const sql = `
-  SELECT code, max(name) as name, 
+  SELECT itype_code_num, max(name) as name,
     CASE
-      WHEN code = 32 THEN 'Equipment'
-      WHEN code = 36 THEN 'Equipment'
-      WHEN code = 27 THEN 'Equipment'
-      WHEN code = 35 THEN 'Equipment'
-      WHEN code = 25 THEN 'Equipment'
-      WHEN code = 87 THEN 'Equipment'
-      WHEN code = 88 THEN 'Equipment'
-      WHEN code = 89 THEN 'Equipment'
-      WHEN code = 90 THEN 'Equipment'
-      WHEN code = 0 THEN 'Books/AV'
-      WHEN code = 1 THEN 'Books/AV'
-      WHEN code = 2 THEN 'Books/AV'
-      WHEN code = 3 THEN 'Books/AV'
-      WHEN code = 4 THEN 'Books/AV'
-      WHEN code = 5 THEN 'Books/AV'
-      WHEN code = 6 THEN 'Books/AV'
-      WHEN code = 118 THEN 'Books/AVs'
-      WHEN code = 10 THEN 'Course Reserves'
-      WHEN code = 24 THEN 'Course Reserves'
-      WHEN code = 11 THEN 'Course Reserves'
-      WHEN code = 12 THEN 'Course Reserves'
+      WHEN itype_code_num = 32 THEN 'Equipment'
+      WHEN itype_code_num = 36 THEN 'Equipment'
+      WHEN itype_code_num = 27 THEN 'Equipment'
+      WHEN itype_code_num = 35 THEN 'Equipment'
+      WHEN itype_code_num = 25 THEN 'Equipment'
+      WHEN itype_code_num = 87 THEN 'Equipment'
+      WHEN itype_code_num = 88 THEN 'Equipment'
+      WHEN itype_code_num = 89 THEN 'Equipment'
+      WHEN itype_code_num = 90 THEN 'Equipment'
+      WHEN itype_code_num = 0 THEN 'Books/AV'
+      WHEN itype_code_num = 1 THEN 'Books/AV'
+      WHEN itype_code_num = 2 THEN 'Books/AV'
+      WHEN itype_code_num = 3 THEN 'Books/AV'
+      WHEN itype_code_num = 4 THEN 'Books/AV'
+      WHEN itype_code_num = 5 THEN 'Books/AV'
+      WHEN itype_code_num = 6 THEN 'Books/AV'
+      WHEN itype_code_num = 118 THEN 'Books/AVs'
+      WHEN itype_code_num = 10 THEN 'Course Reserves'
+      WHEN itype_code_num = 24 THEN 'Course Reserves'
+      WHEN itype_code_num = 11 THEN 'Course Reserves'
+      WHEN itype_code_num = 12 THEN 'Course Reserves'
       ELSE 'Other'
     END AS rec_type
   
-  FROM sierra_view.itype_property_myuser
-
-  GROUP BY code
+  FROM sierra_view.circ_trans
+  LEFT JOIN sierra_view.itype_property_myuser
+    ON sierra_view.itype_property_myuser.code = sierra_view.circ_trans.itype_code_num
+  WHERE substr(item_location_code,1,1) = 'w'
+    AND substr(item_location_code,1,2) != 'wc'
+    AND substr(item_location_code,1,2)!='we'
+  GROUP BY itype_code_num
   `
   return sierra.query(sql)
 }
